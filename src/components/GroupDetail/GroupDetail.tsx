51ad1329ceb06b379a1790b52727535f0a8e2184
Thomas_Pi <pijat.thomas@gmail.com>
2023-10-02 11:04:47 +0200

    Add layout2 on GroupDetail

--- src/components/GroupDetail/GroupDetail.tsx
+++ src/components/GroupDetail/GroupDetail.tsx
@@ -1,6 +1,7 @@
 import { useParams } from "react-router-dom"
 import UserList from "../UserList/UserList"
 import { Button } from "flowbite-react"
+import Layout2 from "../Layouts/Layout2"
 
 
 const GroupDetail = () => {
@@ -9,32 +10,18 @@ const GroupDetail = () => {
 
   return (
     <>
-
-      <div className="text-center p-4">
-
-
-{/* Partie a supprimer, juste la pour afficher des informations récupérer sur la page */}
-        <div className="flex space-x-8">
-          <p className="border-4 border-red-300 rounded-xl p-2 my-2">Role : {role}</p>
-          <p className="border-4 border-red-300 rounded-xl p-2 my-2">Id du groupe : {groupId}</p>
-          <p className="border-4 border-red-300 rounded-xl p-2 my-2">Id du user : {userId}</p>
+      <Layout2>
+        <div className="text-center p-4">
+          {role === "creator" && (
+            <div className="flex space-x-1 my-2">
+              <Button>Supprimer le groupe</Button>
+              <Button>Modifier le groupe</Button>
+              <Button>Modifier les partages</Button>
+            </div>
+          )}
+          <UserList listFor="Members" userId={userId} groupId={groupId} />
         </div>
-
-
-{/* Si le role du User === Creator, alors affiche les bouttons de gestion */}
-        {role === "creator" && (
-          <div className="flex space-x-1 my-2">
-            <Button>Supprimer le groupe</Button>
-            <Button>Modifier le groupe</Button>
-            <Button>Modifier les partages</Button>
-          </div>
-        )}
-
-{/* Affiche la liste des membres */}
-        <UserList listFor="Members" userId={userId} groupId={groupId} />
-
-
-      </div>
+      </Layout2>
     </>
   )
 }
